bb6171aa882e3d2201c70084c7156ac0585aff15
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2025-08-20 15:22:29 -0600

    Create blended_performance.sql

---
+++ models/reporting/blended_performance.sql
@@ -0,0 +1,64 @@
+{{ config (
+    alias = target.database + '_blended_performance'
+)}}
+
+{% set date_granularity_list = ['day', 'week', 'month', 'quarter', 'year'] %}
+  
+WITH initial_amplitude_data as
+  (SELECT 'NYC' as region, date::date as date, COALESCE(SUM(new_york_ny),0) as subscribers
+  FROM {{ source('gsheet_raw','amplitude_orders_byregion') }}
+  GROUP BY 1,2
+  UNION ALL
+  SELECT 'Bay Area' as region, date::date as date, COALESCE(SUM(san_francisco_ca),0) as subscribers
+  FROM {{ source('gsheet_raw','amplitude_orders_byregion') }}
+  GROUP BY 1,2),
+  
+dg_amplitude_data as
+  (SELECT *, {{ get_date_parts('date') }} FROM initial_amplitude_data),
+
+amplitude_data as 
+    ({%- for date_granularity in date_granularity_list %}    
+      SELECT 
+          'Amplitude' as channel,
+          '{{date_granularity}}' as date_granularity,
+          {{date_granularity}} as date,
+          region, 
+          null as campaign_type_default, null as campaign_name,
+          COALESCE(SUM(subscribers),0) as subscribers
+      FROM dg_amplitude_data
+      GROUP BY 1,2,3,4,5,6
+      {% if not loop.last %}UNION ALL
+      {% endif %}
+    {% endfor %}),
+
+paid_data as
+    (SELECT channel, date_granularity, date::date, region, campaign_type_default, campaign_name, 
+        COALESCE(SUM(spend),0) as spend, COALESCE(SUM(clicks),0) as clicks, COALESCE(SUM(impressions),0) as impressions, 
+        COALESCE(SUM(purchases),0) as purchases, COALESCE(SUM(revenue),0) as revenue
+    FROM
+        (SELECT 'Meta' as channel, date, date_granularity, region, campaign_type_default, campaign_name, 
+            spend, link_clicks as clicks, impressions, purchases, revenue
+        FROM {{ source('reporting','facebook_campaign_performance') }}
+        UNION ALL
+        SELECT 'Google Ads' as channel, date, date_granularity, region, campaign_type_default, campaign_name, 
+            spend, clicks, impressions, purchases, revenue
+        FROM {{ source('reporting','googleads_campaign_performance') }}
+        )
+    GROUP BY 1,2,3,4,5,6)
+  
+SELECT channel,
+    date,
+    date_granularity,
+    region,
+    campaign_type_default,
+    campaign_name,
+    spend,
+    clicks,
+    impressions,
+    purchases,
+    revenue,
+    subscribers
+FROM 
+  (SELECT * FROM paid_data
+  UNION ALL 
+  SELECT * FROM amplitude_data)
