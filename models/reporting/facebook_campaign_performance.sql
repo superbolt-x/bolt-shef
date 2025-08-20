8e883e4f2af52f15e56dfa88d2a9281a1224d7ce
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2025-08-20 14:55:55 -0600

    Update facebook_campaign_performance.sql
    CASE WHEN campaign_name ~* '_PRO' THEN 'Campaign Type: Prospecting'
        WHEN campaign_name ~* '_RET' THEN 'Campaign Type: Retargeting'
    END AS campaign_type_default,
    CASE WHEN campaign_name ~* '_NYC' THEN 'NYC'
        WHEN campaign_name ~* '_BayArea' THEN 'Bay Area'
    END AS region,

--- models/reporting/facebook_campaign_performance.sql
+++ models/reporting/facebook_campaign_performance.sql
@@ -6,7 +6,12 @@ SELECT
 campaign_name,
 campaign_id,
 campaign_effective_status,
-campaign_type_default,
+CASE WHEN campaign_name ~* '_PRO' THEN 'Campaign Type: Prospecting'
+    WHEN campaign_name ~* '_RET' THEN 'Campaign Type: Retargeting'
+END AS campaign_type_default,
+CASE WHEN campaign_name ~* '_NYC' THEN 'NYC'
+    WHEN campaign_name ~* '_BayArea' THEN 'Bay Area'
+END AS region,
 date,
 date_granularity,
 spend,
