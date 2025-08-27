f91705be2dd5ec1739b5f5cc207a6a8d8f962848
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2025-08-27 13:47:25 -0600

    Update googleads_asset_group_performance.sql
    landing_page

--- models/reporting/googleads_asset_group_performance.sql
+++ models/reporting/googleads_asset_group_performance.sql
@@ -9,8 +9,9 @@ campaign_id,
 campaign_status,
 campaign_type_default,
 asset_group_name,
-asset_group_id,
+asset_group_id::varchar,
 asset_group_status,
+landing_page,
 date,
 date_granularity,
 spend,
@@ -19,3 +20,5 @@ clicks,
 conversions as purchases,
 conversions_value as revenue
 FROM {{ ref('googleads_performance_by_asset_group') }}
+LEFT JOIN 
+    (SELECT count(*), landing_page, ad_asset_group_id::varchar as asset_group_id FROM {{ source('gsheet_raw','google_landing_pages') }} GROUP BY 2,3) USING(asset_group_id)
